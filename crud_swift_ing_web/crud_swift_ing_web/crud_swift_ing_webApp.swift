0858e6da8f6ff72e1208e52eac338d9b58d800a7
CCDM15 <ccdm15@CCDM15.local>
2022-11-17 09:51:21 -0700

    inicio proyecto
    first commit

---
+++ crud_swift_ing_web/crud_swift_ing_web/crud_swift_ing_webApp.swift
@@ -0,0 +1,20 @@
+//
+//  crud_swift_ing_webApp.swift
+//  crud_swift_ing_web
+//
+//  Created by CCDM15 on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct crud_swift_ing_webApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
